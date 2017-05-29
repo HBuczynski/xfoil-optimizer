cdee9d3cd042f7562ecec62b11a582788e32e228
Hubert <hubert.buczynski94@gmail.com>
2017-05-29 17:11:37 +0200

    Added transform functions and changed the structure of geometry class.

--- application/src/optimizer/geometry.cpp
+++ application/src/optimizer/geometry.cpp
@@ -15,7 +15,9 @@ void Geometry::Load(std::string filename)
         if(!getline(file, name))
             throw std::out_of_range("File containing profile empty");
 
-        points.clear();
+        upperPoints_.clear();
+        lowerPoints_.clear();
+
         while(getline(file,line))
         {
             std::string::size_type sz;
@@ -23,7 +25,11 @@ void Geometry::Load(std::string filename)
             //Can cause exception throws out of range
             x = std::stod(line,&sz);
             y = std::stod(line.substr(sz));
-            points.push_back(Point(x,y));
+
+            if(y >= 0)
+                upperPoints_.push_back(Point(x,y));
+            else
+                lowerPoints_.push_back(Point(x,y));
         }
         file.close();
     }
@@ -32,6 +38,36 @@ void Geometry::Load(std::string filename)
         throw std::invalid_argument("Invalid file path");
     }
 }
+
+void Geometry::InitializeCoefficients()
+{
+    coefficients_.p_u = 0.2;
+    coefficients_.q_u = 0.295;
+    coefficients_.a_u = 1.1;
+    coefficients_.b_u = 2.3;
+    coefficients_.c_u = 0.58;
+    coefficients_.d_u = 1.03;
+
+    coefficients_.p_l = 0.26;
+    coefficients_.q_l = 0.85;
+    coefficients_.a_l = 0.75;
+    coefficients_.b_l = 0.9;
+    coefficients_.c_l = 0.73;
+    coefficients_.d_l = 1.6;
+}
+
+void Geometry::InitializeVectorX()
+{
+    double number = 0;
+    double increaseNumber = 1.0/pointsNumber;
+
+    for(int i=0; i < pointsNumber; ++i)
+    {
+        vectorX_[i] = number;
+        number = number + increaseNumber;
+    }
+
+}
 void Geometry::Save(std::string filename)
 {
     ofstream file;
@@ -39,7 +75,12 @@ void Geometry::Save(std::string filename)
     if (file.is_open())
     {
         file << filename << "\r\n";
-        for(Point a : points)
+        for(Point a : upperPoints_)
+        {
+            file << "     "<< a.x << "    " << a.y << "\r\n";
+        }
+
+        for(Point a : lowerPoints_)
         {
             file << "     "<< a.x << "    " << a.y << "\r\n";
         }
@@ -49,4 +90,36 @@ void Geometry::Save(std::string filename)
     {
         throw std::invalid_argument("Invalid file path");
     }
+}
+
+void Geometry::Transform()
+{
+    //Calculate points based on new coefficients
+    //Musimy wcześniej zdefiniować długość wektora x, tutaj tylko modyfikujemy y
+
+    for(int i=0; i<upperPoints_.size(); ++i)
+    {
+        upperPoints_[i].y = coefficients_.p_u*pow(vectorX_[i], coefficients_.a_u)*pow((1-vectorX_[i]), coefficients_.b_u)+
+                            coefficients_.q_u*pow(vectorX_[i], coefficients_.c_u)*pow((1-vectorX_[i]), coefficients_.d_u);
+    }
+
+    for(int i=0; i<lowerPoints_.size(); ++i)
+    {
+        lowerPoints_[i].y = coefficients_.p_l*pow(vectorX_[i], coefficients_.a_l)*pow((1-vectorX_[i]), coefficients_.b_l)+
+                            coefficients_.q_l*pow(vectorX_[i], coefficients_.c_l)*pow((1-vectorX_[i]), coefficients_.d_l);
+    }
+
+}
+
+const std::vector<Point> Geometry::GetPoints()
+{
+    std::vector<Point> points;
+
+    for(Point a : upperPoints_)
+        points.push_back(a);
+
+    for(Point a : lowerPoints_)
+        points.push_back(a);
+
+    return points;
 }
--- application/src/optimizer/geometry.h
+++ application/src/optimizer/geometry.h
@@ -8,31 +8,86 @@
 #include "xfoil/simulation_results.h"
 
 class Point;
+struct AirfoilCoefficients
+{
+    double p_u;
+    double q_u;
+    double a_u;
+    double b_u;
+    double c_u;
+    double d_u;
+
+    double p_l;
+    double q_l;
+    double a_l;
+    double b_l;
+    double c_l;
+    double d_l;
+
+    AirfoilCoefficients() : p_u(0),
+                        q_u(0),
+                        a_u(0),
+                        b_u(0),
+                        c_u(0),
+                        d_u(0),
+                        p_l(0),
+                        q_l(0),
+                        a_l(0),
+                        b_l(0),
+                        c_l(0),
+                        d_l(0)
+    {
+
+    }
+};
+
 class Geometry
 {
 public:
     Geometry()
     {
+        InitializeCoefficients();
+        InitializeVectorX();
     }
+
     Geometry(std::string filename)
     {
         Load(filename);
+        InitializeCoefficients();
+        InitializeVectorX();
     }
     void Load(std::string filename);
+    void InitializeCoefficients();
+    void InitializeVectorX();
     void Save(std::string filename);
     void Normalze();
     void Transform();
-    const std::vector<Point>& GetPoints()
-    {
-        return points;
-    }
+    const std::vector<Point> GetPoints();
     const SimResults& GetResults()
     {
         return *simResults_;
     }
 
+    Geometry &operator =(const Geometry &data)
+    {
+        upperPoints_ = data.upperPoints_;
+        lowerPoints_ = data.lowerPoints_;
+        vectorX_ = data.vectorX_;
+
+        coefficients_ = data.coefficients_;
+        simResults_  = data.simResults_;
+
+        return *this;
+    }
+
 private:
-    std::vector<Point> points;
+    std::vector<Point> upperPoints_;
+    std::vector<Point> lowerPoints_;
+    std::vector<double> vectorX_;
+
+    AirfoilCoefficients coefficients_;
+    const int pointsNumber = 150;
+
     SimResults *simResults_;
 };
 class Point
@@ -57,3 +112,4 @@ inline bool operator==(const Point& lhs, const Point& rhs)
         return true;
     return false;
 }
+
